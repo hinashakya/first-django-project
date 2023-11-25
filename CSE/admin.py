084bb85f1657de592faff6cc9b0807a976af5b2d
hinashakya <151983464+hinashakya@users.noreply.github.com>
2023-11-25 12:52:22 +0530

    Add files via upload

---
+++ CSE/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models
+from django.contrib import admin
+
+# Register your models 
+
+from CSE.models import Sudent
+
+admin.site.register(Sudent)
